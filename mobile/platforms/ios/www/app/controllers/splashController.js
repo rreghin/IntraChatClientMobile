aae2d8b62655ed1ce67a32a4c22339c5633127f4
Roger Reghin <rreghin@gmail.com>
2014-05-21 16:11:27 -0300

    Primeira versão do cliente mobile.

---
+++ mobile/platforms/ios/www/app/controllers/splashController.js
@@ -0,0 +1,44 @@
+(function () {'use strict';
+
+    var splashController = function ($rootScope, $window, $location, $interval, ClientService) {
+        //debugger;
+        console.log('SplashController');
+        
+        var readyCheck;
+        readyCheck = $interval(function () {
+            //debugger;
+            if ($rootScope.isPhoneGapLoaded) {
+                $interval.cancel(readyCheck);
+                // verifica se o dispositivo tem suporte a WebSockets
+                if ((typeof($window.WebSocket) === 'function') || ('WebSocket' in $window)) {
+                    var milliseconds = 5000, path = '/login';
+
+                    if (!$rootScope.splashing) {
+                        if (ClientService.doNeedLogin()) {
+                            milliseconds = 0;
+                            path = '/login';
+                        } else {
+                            milliseconds = 0;
+                            path = '/main';
+                        }
+                    }
+
+                    $interval(function () {
+                        console.log('indo para ' + path + ' em ' + milliseconds + 'ms');
+                        $location.path(path);
+                    }, milliseconds, 1);
+
+                    $rootScope.splashing = false; // ja mostrou o splash
+                } else {
+                    $window.alert("Error! Your browser does NOT support HTML5 Web Sockets.");
+                }
+            }
+        }, 1000);
+
+    };
+              
+    splashController.$inject = ["$rootScope", "$window", "$location", "$interval", "ClientService"];
+              
+    angular.module("webClient").controller('SplashController', splashController);
+
+}());
